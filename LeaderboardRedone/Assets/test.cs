de2eea06cd34fa615caa20a13fd3103968303d14
DragonIce25 <autobot1984@hotmail.co.uk>
2015-05-06 14:49:13 +0100

    LedaderBoard
    Was more differcult and could only use the one made in github. Need to
    practise making one in the summer

---
+++ LeaderboardRedone/Assets/test.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Had a copy but it crashed however I have used the code from git hub
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
